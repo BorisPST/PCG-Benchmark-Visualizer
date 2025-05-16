d14536db16090e1029ee7229ff201788f7f5e73a
BorisPST <boris.pavic@gmail.com>
2025-05-16 13:49:35 +0200

    Add data files that define the available moves and pokemon (can be extended to include more later on)

---
+++ pcgbenchmark-visualizer/pokemonbattle-problem/pokemon-logic/data/moves.py
@@ -0,0 +1,53 @@
+from ..utils.move_utils import PokemonMove, MoveCategory
+from ..utils.type_utils import PokemonType
+
+# Here we define the moves available in the simulation.
+
+Tackle = PokemonMove(
+    name="Tackle",
+    power=40,
+    accuracy=100,
+    category=MoveCategory.PHYSICAL,
+    type=PokemonType.NORMAL
+)
+
+Scratch = PokemonMove(
+    name="Scratch",
+    power=40,
+    accuracy=100,
+    category=MoveCategory.PHYSICAL,
+    type=PokemonType.NORMAL
+)
+
+VineWhip = PokemonMove(
+    name="Vine Whip",
+    power=40,
+    accuracy=100,
+    category=MoveCategory.PHYSICAL,
+    type=PokemonType.GRASS
+)
+
+Ember = PokemonMove(
+    name="Ember",
+    power=40,
+    accuracy=100,
+    category=MoveCategory.SPECIAL,
+    type=PokemonType.FIRE
+)
+
+WaterGun = PokemonMove(
+    name="Water Gun",
+    power=40,
+    accuracy=100,
+    category=MoveCategory.SPECIAL,
+    type=PokemonType.WATER
+)
+
+# For convenience, we make a dictionary of all moves, for easy access baed on name
+MOVES = {
+    "Tackle": Tackle,
+    "Scratch": Scratch,
+    "Vine Whip": VineWhip,
+    "Ember": Ember,
+    "Water Gun": WaterGun
+}
---
+++ pcgbenchmark-visualizer/pokemonbattle-problem/pokemon-logic/data/pokemon.py
@@ -0,0 +1,67 @@
+from ..utils.pokemon_utils import PokemonBlueprint
+from ..utils.type_utils import PokemonType
+from .moves import MOVES
+from ..utils.stats_utils import PokemonStats
+
+# Here we define the Pokemon (blueprints) available in the simulation.
+
+Bulbasaur = PokemonBlueprint(
+    name="Bulbasaur",
+    type1=PokemonType.GRASS,
+    type2=PokemonType.POISON,
+    base_stats=PokemonStats(
+        hp=45,
+        attack=49,
+        defense=49,
+        special_attack=65,
+        special_defense=65,
+        speed=45
+    ),
+    move_pool={
+        1: MOVES["Tackle"],
+        3: MOVES["Vine Whip"],
+    }
+)
+
+Charmander = PokemonBlueprint(
+    name="Charmander",
+    type1=PokemonType.FIRE,
+    type2=None,
+    base_stats=PokemonStats(
+        hp=39,
+        attack=52,
+        defense=43,
+        special_attack=60,
+        special_defense=50,
+        speed=65
+    ),
+    move_pool={
+        1: MOVES["Scratch"],
+        3: MOVES["Ember"],
+    }
+)
+
+Squirtle = PokemonBlueprint(
+    name="Squirtle",
+    type1=PokemonType.WATER,
+    type2=None,
+    base_stats=PokemonStats(
+        hp=44,
+        attack=48,
+        defense=65,
+        special_attack=50,
+        special_defense=64,
+        speed=43
+    ),
+    move_pool={
+        1: MOVES["Tackle"],
+        3: MOVES["Water Gun"],
+    }
+)
+
+# For convenience, we make a dictionary of all Pokemon (blueprints), for easy access based on name
+POKEMON = {
+    "Bulbasaur": Bulbasaur,
+    "Charmander": Charmander,
+    "Squirtle": Squirtle
+}
